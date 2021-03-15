d4b11b1d28fa09921cb90a4ffce052bb2e4b9647
Hari <hari.prasad@vedavit-ps.com>
2021-03-15 15:54:52 +0530

    Add NaiveBaysian Classifer & Numerology notebooks

---
+++ PythonPackages/calc.py
@@ -0,0 +1,21 @@
+# calc.py>
+
+# functions defined in calc.py
+def addNumbers(a, b):
+    print("Sum is ", a + b)
+
+
+def subtractNumbers(a, b):
+    print("Differnece is ", a - b)
+
+
+def multiplyNumbers(a, b):
+    print("Product is ", a * b)
+
+
+def divideNumbers(a, b):
+    print("Division is ", a / b)
+
+
+def modulusNumbers(a, b):
+    print("Remainder is ", a % b)
---
+++ PythonPackages/main.py
@@ -0,0 +1,21 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from calc import addNumbers, divideNumbers, multiplyNumbers, subtractNumbers, modulusNumbers
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    addNumbers(10,20)
+    subtractNumbers(25, 6)
+    multiplyNumbers(25, 6)
+    divideNumbers(300,15)
+    modulusNumbers(25, 6)
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
